d535cffb112090a3e0cc41dd7ac1c21a8fc20de7
Kwanit Gupta <gupta.45@iitj.ac.in>
2023-02-14 17:09:35 +0530

    Update cifar10_config.py

--- cifar10_config.py
+++ cifar10_config.py
@@ -13,7 +13,22 @@ model_configs = {
     model_name(SENet18): (SENet18, '/content/gdrive/MyDrive/Public Checkpoints/senet18_epoch_279_acc_94.59.pth')
 }
 
+def attack_name(attack):
+    return attack.__name__  
+         
+attack_configs ={ 
+    attack_name(ifgsm): wrap_attack(ifgsm, {'learning_rate': 0.002, 'epsilon': 0.03}),
+    attack_name(momentum_ifgsm): wrap_attack(momentum_ifgsm, {'learning_rate': 0.002, 'epsilon': 0.03, 'decay': 0.9}),
+    attack_name(fgsm): wrap_attack(fgsm, {}),
+    attack_name(deepfool): wrap_attack(deepfool, {}), 
+    attack_name(CW_Linf): wrap_cw_linf(CW_Linf, {'targeted': False} ),
+    attack_name(Transferable_Adversarial_Perturbations): wrap_attack(Transferable_Adversarial_Perturbations, {'learning_rate': 0.002, 'epsilon': 0.03,  'lam' : 0.005, 'alpha' : 0.5, 's' : 3, 'yita' : 0.01} ),
+}
 
+use_projection = True
+
+ILA_params = {
+    attack_name(ifgsm): {'niters': 10, 'learning_rate': 0.006, 'epsilon':0.03, 'coeff': 5.0}, 
     attack_name(momentum_ifgsm): {'niters': 10, 'learning_rate': 0.006, 'epsilon':0.03, 'coeff': 5.0},
     attack_name(fgsm):  {'niters': 10, 'learning_rate': 0.006, 'epsilon':0.03, 'coeff': 5.0}, 
     attack_name(deepfool): {'niters': 10, 'learning_rate': 1.0, 'epsilon':0.03, 'coeff': 5.0},
