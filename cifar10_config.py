a5aa7941a8d44dfdfcae831ec2f4c9c3fae0efbc
Kwanit Gupta <gupta.45@iitj.ac.in>
2023-02-14 16:51:38 +0530

    Update cifar10_config.py

--- cifar10_config.py
+++ cifar10_config.py
@@ -7,30 +7,13 @@ def model_name(model):
     return model.__name__
 
 model_configs = {
-    model_name(ResNet18): (ResNet18, '/share/cuvl/weights/cifar10/resnet18_epoch_347_acc_94.77.pth'),
-    model_name(DenseNet121): (DenseNet121,'/share/cuvl/weights/cifar10/densenet121_epoch_315_acc_95.61.pth'),
-    model_name(GoogLeNet): (GoogLeNet, '/share/cuvl/weights/cifar10/googlenet_epoch_227_acc_94.86.pth'),
-    model_name(SENet18): (SENet18, '/share/cuvl/weights/cifar10/senet18_epoch_279_acc_94.59.pth')
+    model_name(ResNet18): (ResNet18, '/content/gdrive/MyDrive/Public Checkpoints/resnet18_epoch_347_acc_94.77.pth'),
+    model_name(DenseNet121): (DenseNet121,'/content/gdrive/MyDrive/Public Checkpoints/densenet121_epoch_315_acc_95.61.pth'),
+    model_name(GoogLeNet): (GoogLeNet, '/content/gdrive/MyDrive/Public Checkpoints/googlenet_epoch_227_acc_94.86.pth'),
+    model_name(SENet18): (SENet18, '/content/gdrive/MyDrive/Public Checkpoints/senet18_epoch_279_acc_94.59.pth')
 }
 
 
-def attack_name(attack):
-    return attack.__name__  
-         
-attack_configs ={ 
-    attack_name(ifgsm): wrap_attack(ifgsm, {'learning_rate': 0.002, 'epsilon': 0.03}),
-    attack_name(momentum_ifgsm): wrap_attack(momentum_ifgsm, {'learning_rate': 0.002, 'epsilon': 0.03, 'decay': 0.9}),
-    attack_name(fgsm): wrap_attack(fgsm, {}),
-    attack_name(deepfool): wrap_attack(deepfool, {}), 
-    attack_name(CW_Linf): wrap_cw_linf(CW_Linf, {'targeted': False} ),
-    attack_name(Transferable_Adversarial_Perturbations): wrap_attack(Transferable_Adversarial_Perturbations, {'learning_rate': 0.002, 'epsilon': 0.03,  'lam' : 0.005, 'alpha' : 0.5, 's' : 3, 'yita' : 0.01} ),
-}
-
-
-use_projection = True
-
-ILA_params = {
-    attack_name(ifgsm): {'niters': 10, 'learning_rate': 0.006, 'epsilon':0.03, 'coeff': 5.0}, 
     attack_name(momentum_ifgsm): {'niters': 10, 'learning_rate': 0.006, 'epsilon':0.03, 'coeff': 5.0},
     attack_name(fgsm):  {'niters': 10, 'learning_rate': 0.006, 'epsilon':0.03, 'coeff': 5.0}, 
     attack_name(deepfool): {'niters': 10, 'learning_rate': 1.0, 'epsilon':0.03, 'coeff': 5.0},
@@ -44,8 +27,3 @@ source_layers = {
     model_name(GoogLeNet): list(enumerate(GoogLeNet()._modules.keys())), 
     model_name(SENet18):list(enumerate(SENet18()._modules.keys())), 
 }
-                
-
-
-
- 
